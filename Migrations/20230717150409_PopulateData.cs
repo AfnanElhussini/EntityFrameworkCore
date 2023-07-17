07419828c99fed6ce4db7bc9aef2221be4adf11f
AfnanElhussini <afnanrlhussini23@gmail.com>
2023-07-17 18:11:12 +0300

    Migration PopulateData is Added

---
+++ Migrations/20230717150409_PopulateData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert Into Employees Values ('Afnan')");
+         
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete From Employess Where EmployeeName = 'Afnan'");
+        }
+    }
+}
