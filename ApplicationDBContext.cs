4a1997f2a372aa8047d6915990ac2e49e56fb417
AfnanElhussini <afnanrlhussini23@gmail.com>
2023-07-17 18:02:01 +0300

    First Commit

---
+++ ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore
+{
+    public class ApplicationDBContext : DbContext
+    {
+        // Connection String is degined in OnConfiguring method
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=EFDataBase;Integrated Security=True; TrustServerCertificate=True");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var _context = new ApplicationDBContext();
+            var employee = new Employee
+            {
+                EmployeeName = "Employee 1"
+            };
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+            
+        }
+    }
+}
