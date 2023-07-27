6b7de73128110a426f10c7427455fa94c5b03cfa
AfnanElhussini <afnanrlhussini23@gmail.com>
2023-07-28 02:18:17 +0300

    Indexes

---
+++ Migrations/20230727231407_AddIndex.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Blogs",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_Url",
+                table: "Blogs",
+                column: "Url");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_Url",
+                table: "Blogs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Blogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
